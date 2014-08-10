45d29fd99baaadfe839588831298c21487742b51
Losses <losses.don@gmail.com>
2014-08-10 22:23:52 +0800

    增加了个托盘图标

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
+var windowIsShow = true;
+
 var gui = require('nw.gui');
 var clipboard = gui.Clipboard.get();
 var win = gui.Window.get();
 
 //win.x = screen.width - 200;
 //win.y = 50;
+
+function switchWindow() {
+    if (windowIsShow)
+        win.hide();
+    else {
+        win.show();
+        win.focus();
+    }
+}
 
 function enlargeWindow() {
     $("#program_icon").hide();
@@ -19,29 +30,47 @@ function narrowingWindow() {
     win.height = 39;
 }
 
+//注册快捷键
+var option = {
+    key: "Alt+Q",
+    active: function() {
+        console.log("Global desktop keyboard shortcut: " + this.key + " active.");
+    },
+    failed: function(msg) {
+        console.log(msg);
+    }
+};
+
+var shortcut = new gui.Shortcut(option);
+
+gui.App.registerGlobalHotKey(shortcut);
+
+shortcut.on('active', function() {
+    switchWindow();
+});
+
+//注册托盘图标
+
+var menu = new gui.Menu();
+menu.append(new gui.MenuItem({
+    label: 'Exit',
+    click: function() {
+        return win.close();
+    }
+}));
+
+var tray = new gui.Tray({
+    title: 'DStuarch',
+    tooltip: '打开DStuarch',
+    icon: './images/tray.png',
+    menu: menu
+});
+
+tray.on('click', function() {
+    switchWindow();
+});
+
 $(document).ready(function() {
-
-    var option = {
-        key: "Alt+Q",
-        active: function() {
-            console.log("Global desktop keyboard shortcut: " + this.key + " active.");
-        },
-        failed: function(msg) {
-            console.log(msg);
-        }
-    };
-
-    var shortcut = new gui.Shortcut(option);
-
-    gui.App.registerGlobalHotKey(shortcut);
-
-    shortcut.on('active', function() {
-        win.show();
-        win.focus();
-    });
-
-
-
     $.getJSON("list.json", function(data) {
         $.each(data.emotions, function(i, item) {
             $("#emotion_box").append(
