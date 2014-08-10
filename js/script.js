037d162af43cf94165b03a50b828ca87ce079283
Losses <losses.don@gmail.com>
2014-08-10 19:40:45 +0800

    解决了点击部分颜文字按钮会造成程序崩溃的问题

--- js/script.js
+++ js/script.js
@@ -5,16 +5,6 @@ var win = gui.Window.get();
 win.x = screen.width - 200;
 win.y = 50;
 
-function loadInfo() {
-    $.getJSON("list.json", function(data) {
-        $.each(data.emotions, function(i, item) {
-            $("#emotion_box").append(
-                    "<button class=emotion onclick=emotion(\"" + item.content + "\")>" + item.content + "</button>"
-                    );
-        });
-    });
-}
-
 function enlargeWindow() {
     $("#program_icon").hide();
     win.x = win.x - 138;
@@ -29,21 +19,29 @@ function narrowingWindow() {
     win.height = 39;
 }
 
-function emotion(emotion_i) {
-    clipboard.set(emotion_i, 'text');
-    narrowingWindow();
-}
-;
-
 $(document).ready(function() {
+
+    $.getJSON("list.json", function(data) {
+        $.each(data.emotions, function(i, item) {
+            $("#emotion_box").append(
+                    "<button class=\"emotion\">" + item.content + "</button>"
+                    );
+
+            //绑定内容
+            $(".emotion").on("click", function() {
+                var emotionContent = $(this).html();
+                clipboard.set(emotionContent, 'text');
+                narrowingWindow();
+            });
+
+        });
+    });
 
     $("#elf").on("mouseenter", function() {
         enlargeWindow();
     });
 
     $("s").on("mouseover", function() {
-        narrowingWindow();
+        //narrowingWindow();
     });
-
-    loadInfo();
 });
