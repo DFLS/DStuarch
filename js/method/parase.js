4ab79601d7039ed73091bd5e5415e53bba51207e
Losses <losses.don@gmail.com>
2014-08-14 23:02:20 +0800

    修正了颜文字分组功能

--- js/method/parase.js
+++ js/method/parase.js
@@ -1,19 +1,22 @@
 var parase = {
-    
     //写入颜文字方法
     writeEmotions: function(tagId) {
         //清空现有颜文字
         $("#emotion_box").html("");
 
         for (x in emotionData[tagId]) {
+            if (!emotionData[tagId][x].revert)
+                var buttonContent = emotionData[tagId][x].content;
+            else
+                var buttonContent = emotionData[tagId][x].name;
             $("#emotion_box").append(
-                    "<button class=\"emotion\">" + emotionData[tagId][x].content + "</button>"
+                    '<button class="emotion" content="' + emotionData[tagId][x].content + '">' + buttonContent + '</button>'
                     );
         }
 
         //重新绑定事件
         $(".emotion").on("click", function() {
-            var emotionContent = $(this).text();
+            var emotionContent = $(this).attr("content");
             clipboard.set(emotionContent, 'text');
             win.hide();
             windowIsShow = false;
